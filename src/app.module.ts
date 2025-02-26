6ac4175e8cf6e948beaa531ba4574ebf06b30adc
kledenai <bruno@khaneland.com>
2025-02-26 05:29:13 -0300

    Adding new modules in App

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,28 @@
 import { Module } from '@nestjs/common';
-import { AppService } from 'src/app.service';
 import { PrismaModule } from 'prisma/prisma.module';
+import { AuthModule } from 'modules/auth/auth.module';
 import { UserModule } from './modules/user/user.module';
+import { CartModule } from './modules/cart/cart.module';
+import { OrderModule } from './modules/order/order.module';
 import { ProductModule } from 'modules/product/product.module';
 import { CategoryModule } from 'modules/category/category.module';
-import { CartModule } from './modules/cart/cart.module';
+import { CartItemModule } from './modules/cart-item/cart-item.module';
+import { OrderItemModule } from './modules/order-item/order-item.module';
+import { ShippingAddressModule } from './modules/shipping-address/shipping-address.module';
 
 @Module({
-  imports: [PrismaModule, ProductModule, UserModule, CategoryModule, CartModule],
+  imports: [
+    AuthModule,
+    UserModule,
+    CartModule,
+    OrderModule,
+    PrismaModule,
+    ProductModule,
+    CategoryModule,
+    CartItemModule,
+    OrderItemModule,
+    ShippingAddressModule
+  ],
   controllers: [],
   providers: [],
 })
