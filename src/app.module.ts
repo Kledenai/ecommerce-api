c364a23d3729094f197c4f6f63d1caec4934698b
kledenai <bruno@khaneland.com>
2025-02-26 02:36:21 -0300

    Main module configuration: Changes in app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppService } from 'src/app.service';
+import { AppController } from 'src/app.controller';
+import { PrismaModule } from 'prisma/prisma.module';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { ProductModule } from 'modules/product/product.module';
+import { ProductService } from 'modules/product/product.service';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule, ProductModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService, ProductService],
 })
 export class AppModule {}
