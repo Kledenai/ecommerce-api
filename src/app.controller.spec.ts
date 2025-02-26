785089ec8cd7f0d9c857c408cbc8be8053523cd8
kledenai <bruno@khaneland.com>
2025-02-26 02:36:13 -0300

    Tests: Added or adjusted tests for AppController

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 
 describe('AppController', () => {
   let appController: AppController;
+  let appService: AppService;
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
@@ -12,11 +13,25 @@ describe('AppController', () => {
     }).compile();
 
     appController = app.get<AppController>(AppController);
+    appService = app.get<AppService>(AppService);
   });
 
   describe('root', () => {
     it('should return "Hello World!"', () => {
       expect(appController.getHello()).toBe('Hello World!');
+    });
+  });
+
+  describe('getHelloServiceCall', () => {
+    it('should call the AppService and return the correct message', () => {
+      const appServiceMock = jest.spyOn(appService, 'getHello').mockReturnValue('Hello from service!');
+
+      const result = appController.getHello();
+
+      expect(appServiceMock).toHaveBeenCalled();
+      expect(result).toBe('Hello from service!');
+
+      appServiceMock.mockRestore();
     });
   });
 });
