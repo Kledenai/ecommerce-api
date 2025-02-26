9367a543b0fa389048d5a9bcbdd58ffa6360b840
kledenai <bruno@khaneland.com>
2025-02-26 02:36:46 -0300

    Product: Added or modified the product controller

---
+++ src/modules/product/product.controller.ts
@@ -0,0 +1,52 @@
+import {
+  Get,
+  Put,
+  Post,
+  Body,
+  Param,
+  Delete,
+  Controller,
+} from '@nestjs/common';
+import { ProductService } from 'modules/product/product.service';
+
+@Controller('products')
+export class ProductController {
+  constructor(private readonly productService: ProductService) {}
+
+  @Post()
+  async createProduct(
+    @Body()
+    body: {
+      name: string;
+      price: number;
+      description?: string;
+      imageUrl: string;
+      category: string;
+    },
+  ) {
+    return this.productService.createProduct(body);
+  }
+
+  @Get()
+  async getProducts() {
+    return this.productService.getProducts();
+  }
+
+  @Get(':id')
+  async getProductById(@Param('id') id: number) {
+    return this.productService.getProductById(id);
+  }
+
+  @Put(':id')
+  async updateProduct(
+    @Param('id') id: number,
+    @Body() body: { name?: string; price?: number; description?: string },
+  ) {
+    return this.productService.updateProduct(id, body);
+  }
+
+  @Delete(':id')
+  async deleteProduct(@Param('id') id: number) {
+    return this.productService.deleteProduct(id);
+  }
+}
