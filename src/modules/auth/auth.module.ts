cb90ca4d8e1e296e68cd3273f8f239df1d31a9f0
kledenai <bruno@khaneland.com>
2025-03-05 08:23:18 -0300

    adding jwt contants

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,24 +1,21 @@
-import { JwtModule } from '@nestjs/jwt';
-import { forwardRef, Module } from '@nestjs/common';
-import { PrismaModule } from 'prisma/prisma.module';
-import { UserModule } from 'modules/user/user.module';
+import { AuthController } from 'modules/auth/auth.controller';
 import { AuthService } from 'modules/auth/auth.service';
 import { JwtStrategy } from 'modules/auth/jwt.strategy';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { AuthController } from 'modules/auth/auth.controller';
+import { UserModule } from 'modules/user/user.module';
+import { jwtConstants } from 'modules/auth/constants';
+import { forwardRef, Module } from '@nestjs/common';
+import { PrismaModule } from 'prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     PrismaModule,
     forwardRef(() => UserModule),
-    JwtModule.registerAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: async (configService: ConfigService) => ({
-        secret: configService.get<string>('JWT_SECRET') || 'fallback_secret',
-        signOptions: { expiresIn: '1h' },
-      }),
+    JwtModule.register({
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: jwtConstants.expiresIn },
     }),
   ],
   providers: [AuthService, JwtStrategy],
